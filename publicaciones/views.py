bd955ad26de899b0ae078be0a93ae5ffcd1829c1
d-moya <dayanazdiaz@gmail.com>
2025-10-27 20:15:25 -0300

    publicaciones
    estan listos los templates de publicaciones

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 def muro_prestamos_view(request):
-
-    return render(request, 'publicaciones/muro_base.html', {'muro_nombre': 'Préstamos'})
+    return render(request, 'Muros/muro_prestamo.html')
 
 def muro_ayudas_view(request):
-
-    return render(request, 'publicaciones/muro_base.html', {'muro_nombre': 'Ayudas'})
+    return render(request, 'Muros/muro_ayudas.html')
 
 def muro_servicio_view(request):
+    return render(request, 'Muros/muro_tutorias.html')
 
-    return render(request, 'publicaciones/muro_base.html', {'muro_nombre': 'Servicio'})
+@login_required
+def home_publicar_view(request):
+    return render(request,'publicar/inicio_publicar.html')
 
-@login_required 
-def crear_publicacion_view(request):
-    return render(request, 'publicaciones/crear_publicacion.html')
+def publicar_ayudas_view(request):
+    return render(request,'publicar/publicar_ayudas.html')
+
+def publicar_prestamos_view(request):
+    return render(request,'publicar/publicar_prestamo.html')
+
+def publicar_tutorias_view(request):
+    return render(request,'publicar/publicar_tutoria.html')
