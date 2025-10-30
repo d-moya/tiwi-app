65f086c1df8da2342663b40cb522353521227f0a
Felipe-375 <felipealpeco@gmail.com>
2025-10-30 13:08:59 -0300

    Semi reparacion links
    Aun falta los tkns de seguridad

--- publicaciones/urls.py
+++ publicaciones/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('formulario/ayudas', views.publicar_ayudas_view, name='form_ayudas'),
     path('formulario/prestamos', views.publicar_prestamos_view, name='form_prestamo'),
     path('formulario/servicio', views.publicar_tutorias_view, name='form_tutoria'),
+    path('Encuesta/ayudas', views.publicar_ayudas_view, name='preferencias_ayudas'),
+    path('Encuesta/prestamos', views.publicar_prestamos_view, name='preferencias_prestamo'),
+    path('Encuesta/servicio', views.publicar_tutorias_view, name='preferencias_tutoria'),
 ]
--- publicaciones/views.py
+++ publicaciones/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import FormAyudas, FormTutoria, FormPrestamos 
-from .models import Publicacion
 
 def validar(request, form_class, tipo_muro_valor, redirect_name, template_name):
     if request.method == 'POST':
@@ -21,36 +20,28 @@ def home_publicar_view(request):
     return render(request,'publicar/inicio_publicar.html')
 
 def publicar_ayudas_view(request):
-    return validar(request,FormAyudas,'AYUDAS', 'muro_ayudas', 'publicar/publicar_ayudas.html')
+    return validar(request,FormAyudas,'AYUDAS', 'muro_ayudas', 'publicar/Encuesta_ayudantia.html')
 
 def publicar_prestamos_view(request):
-    return validar(request,FormPrestamos,'PRESTAMOS', 'muro_prestamo', 'publicar/publicar_prestamo.html')
+    return validar(request,FormPrestamos,'PRESTAMOS', 'muro_prestamo', 'publicar/Encuesta_prestamo.html')
 
 def publicar_tutorias_view(request):
-    return validar(request,FormTutoria,'SERVICIOS', 'muro_tutoria', 'publicar/publicar_tutoria.html')
+    return validar(request,FormTutoria,'SERVICIOS', 'muro_tutoria', 'publicar/Encuesta_tutoria.html')
 
 def muro_prestamos_view(request):
-    publicaciones = Publicacion.objects.filter(tipoMuro = 'PRESTAMOS').order_by('fechaCreacion')
-    datitos = {
-        'nombre' : 'Prestamos',
-        'publicacion' : publicaciones,
-    }
-    return render(request, 'Muros/muro_prestamo.html', datitos)
-
+    return render(request, 'Muros/muro_prestamo.html', {'muro_nombre': 'Préstamos'})
 
 def muro_ayudas_view(request):
-    publicaciones = Publicacion.objects.filter(tipoMuro = 'AYUDAS').order_by('fechaCreacion')
-    datitos = {
-        'nombre' : 'Ayudas',
-        'publicacion' : publicaciones,
-    }
-    return render(request, 'Muros/muro_ayudas.html', datitos)
-
+    return render(request, 'Muros/muro_ayudas.html', {'muro_nombre': 'Ayudas'})
 
 def muro_servicio_view(request):
-    publicaciones = Publicacion.objects.filter(tipoMuro = 'SERVICIOS').order_by('-fechaCreacion')
-    datitos = {
-        'nombre' : 'Servicios',
-        'publicacion' : publicaciones,
-    }
-    return render(request, 'Muros/muro_tutoria.html', datitos)
+    return render(request, 'Muros/muro_tutoria.html', {'muro_nombre': 'Servicio'})
+
+def muro_servicio_view(request):
+    return render(request, 'publicar/preferencias_tutoria.html')
+
+def muro_servicio_view(request):
+    return render(request, 'publicar/preferencias_ayudas.html')
+
+def muro_servicio_view(request):
+    return render(request, 'publicar/preferencias_prestamo.html')
