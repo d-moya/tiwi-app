cb3028f7c4aed3c73b3a62126e961bc97327637e
d-moya <dayanazdiaz@gmail.com>
2025-11-04 20:49:23 -0300

    modelo perfil usuarios
    empece el perfil de usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .models import Publicacion
 
 class perfilUsuario(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
