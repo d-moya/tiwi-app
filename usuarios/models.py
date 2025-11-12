596dd7133de2c67afb8e0355a41b1cf1e4a1f17a
d-moya <dayanazdiaz@gmail.com>
2025-11-11 22:52:19 -0300

    perfil usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from .models import Publicacion
 
-#class perfilUsuario(models.Model):
- # usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-  # fotoPerfil = models.ImageField()
-   # fechaUnion = models.DateField()
-   # Carrera :
-   # Redict a una pestaña de perfil, mostrar una imagen predeterminada, opion cambio imagen
+class perfilUsuario(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    fotoPerfil = models.ImageField(upload_to='usuarios_fotos/', blank=True, null=True)
+    fechaUnion = models.DateField(auto_now_add=True)
+    Carrera = models.CharField(max_length=200, blank=True, null = True)
+    #Redict a una pestaña de perfil, mostrar una imagen predeterminada, opion cambio imagen
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('registro/', views.registro_usuario, name='registro'),
+    path('perfil/', views.perfilusuario, name='perfilUsuario'),
+    path('perfil/editar/', views.editarPerfil, name='editarPerfil'),
 ]
